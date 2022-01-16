cf34ef85c8691137f7d23b567ae3813abfe872de
Mark Griffin <magriffin@chapman.edu>
2022-01-16 23:14:09 +0000

    Implemennted parsing methods for everything but the transition function

--- Parser.cpp
+++ Parser.cpp
@@ -22,12 +22,10 @@ Parser::~Parser()
 void Parser::parseNFAFile(string nfa)
 {
     // Do things using helper methods
-    string statesLine = getNextLine(nfa);
-    parseStates(statesLine);
-
-    cout << getNextLine(nfa) << "\n";
-    cout << getNextLine(nfa) << "\n";
-    cout << getNextLine(nfa) << "\n";
+    parseStates(getNextLine(nfa));
+    parseAlphabet(getNextLine(nfa));
+    parseStartState(getNextLine(nfa));
+    parseAcceptStates(getNextLine(nfa));
 }
 
 // Parses the states from the line of text contiaining the states
@@ -44,6 +42,7 @@ void Parser::parseStates(string stateLine)
         states.push_back(stateLine.at(i));
     }
 
+    // TODO: Delete once finished
     // Testing Only
     for (int i = 0; i < states.size(); ++i)
     {
@@ -51,6 +50,74 @@ void Parser::parseStates(string stateLine)
     }
 }
 
+// Parses the alphabet from the line of text containing the alphabet
+void Parser::parseAlphabet(string alphabetLine)
+{
+    for (int i = 0; i < alphabetLine.length(); ++i)
+    {
+        if ((alphabetLine.at(i) == '\t'))
+        {
+            continue;
+        }
+
+        alphabet.push_back(alphabetLine.at(i));
+    }
+
+    // TODO: Delete once finished
+    // Testing only
+    for (int i = 0; i < alphabet.size(); ++i)
+    {
+        cout << "Symbol: " << alphabet[i] << "\n";
+    }
+}
+
+// Parse the start state from the line of text containing the start state
+void Parser::parseStartState(string startStateLine)
+{
+    for (int i = 0; i < startStateLine.length(); ++i)
+    {
+        if ((startStateLine.at(i) == '{') || (startStateLine.at(i) == '}'))
+        {
+            continue;
+        }
+        else if (find(states.begin(), states.end(), startStateLine.at(i)) == states.end()) // Check to see if the start state is actually a valid state
+        {
+            cout << "Specified start state does not exist, please check the file and try again" << '\n';
+            return;
+        }
+
+        startState = startStateLine.at(i);
+    }
+
+    // TODO: Delete once finished
+    cout << "Start State: " << startState << "\n";
+}
+
+// Parse the accept states from the line of text containg the accept states
+void Parser::parseAcceptStates(string acceptStateLine)
+{
+    for (int i = 0; i < acceptStateLine.length(); ++i)
+    {
+        if ((acceptStateLine.at(i) == '{') || (acceptStateLine.at(i) == '}') || (acceptStateLine.at(i) == '\t'))
+        {
+            continue;
+        }
+        else if (find(states.begin(), states.end(), acceptStateLine.at(i)) == states.end()) // Check to see if the accept state is a valid state
+        {
+            cout << '{' << acceptStateLine.at(i) << '}' << " is not a valid state. Please check the file and try again" << '\n';
+            continue;
+        }
+
+        acceptStates.push_back(acceptStateLine.at(i));
+    }
+
+    // TODO: Delete once done
+    // Testing only
+    for (int i = 0; i < acceptStates.size(); ++i)
+    {
+        cout << "Accept State: " << acceptStates[i] << '\n';
+    }
+}
 
 // Gets the next line of text needed for parsing from the string of text
 string Parser::getNextLine(string text)
@@ -62,7 +129,6 @@ string Parser::getNextLine(string text)
     {
         if (text.at(i) == '\n')
         {
-            cout << "newline" << "\n";
             newlineIndex = i + 1; // Sets the newline index to 1 more than where the newline char was found
             break;
         }
